8bc47cc1b466ba7f5b1b9b807c0ca7f77eea89eb
Tschonitsch <brotscho@gmail.com>
2026-05-01 14:52:14 +0200

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
     [_FUNC] = LAYOUT(
         KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                                  KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
-        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,
-        KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                   KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
+        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_UP,      KC_O,       KC_P,       KC_LBRC,
+        KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                   KC_H,       KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_SCLN,    KC_QUOT,
         KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       MS_LEFT,        MS_UP,      KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
             KC_LGUI,    XXXXXXX,    KC_LALT,    XXXXXXX,    MS_BTN1,                                             MS_BTN2,     TG(_FUNC),    KC_RCTL,     KC_RALT,    KC_RGUI
     )
