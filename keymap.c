55cc2f4ab82ba41696d2b6a2a37497cbf81cd40d
Tschonitsch <brotscho@gmail.com>
2026-05-07 19:43:04 +0200

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,
         KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                   KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
         KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_VOLD,        XXXXXXX,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
-            KC_LGUI,    XXXXXXX,    KC_LALT,    XXXXXXX,    KC_SPC,                                              KC_ENT,     TG(_FUNC),    KC_RCTL,     KC_RALT,    KC_RGUI
+            XXXXXXX,    KC_LGUI,     XXXXXXX,    KC_LALT,     KC_SPC,                                              KC_ENT,     KC_RALT,    KC_RCTL,     KC_RGUI,    TG(_FUNC)
     ),
 
 
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_UP,      KC_O,       KC_P,       KC_LBRC,
         KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                   KC_H,       KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_SCLN,    KC_QUOT,
         KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       MS_LEFT,        MS_UP,      KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
-            KC_LGUI,    XXXXXXX,    KC_LALT,    XXXXXXX,    MS_BTN1,                                             MS_BTN2,     TG(_FUNC),    KC_RCTL,     KC_RALT,    KC_RGUI
+            XXXXXXX,    KC_LGUI,     XXXXXXX,    KC_LALT,     MS_BTN1,                                              MS_BTN2,     KC_RALT,    KC_RCTL,     KC_RGUI,    TG(_FUNC)
     )
 };
 
